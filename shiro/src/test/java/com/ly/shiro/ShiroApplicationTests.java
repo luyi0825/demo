86cb69835455b2341c6cb154078cd4ec8a464400
ly <893431435@qq.com>
2020-07-14 21:02:32 +0800

    提交代码

---
+++ shiro/src/test/java/com/ly/shiro/ShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
