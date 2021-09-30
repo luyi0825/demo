2298359e9de8152442a22a33eb386a4e5b821164
luyi <893431435@qq.com>
2021-09-30 14:50:10 +0800

    feat:rabbit-simple

---
+++ mq/rocketmq/simple-example/src/main/java/com/demo/rocketmq/simple/AsyncProducer.java
@@ -0,0 +1,60 @@
+package com.demo.rocketmq.simple;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author luyi
+ * 同步生产者
+ */
+public class AsyncProducer {
+
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new DefaultMQProducer("test");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("192.168.3.222:9876");
+        //Launch the instance.
+        producer.start();
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+
+        int messageCount = 100;
+        final CountDownLatch countDownLatch = new CountDownLatch(messageCount);
+        for (
+                int i = 0;
+                i < messageCount; i++) {
+            try {
+                final int index = i;
+                Message msg = new Message("TopicTest",
+                        "TagA",
+                        "OrderID188",
+                        "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+                producer.send(msg, new SendCallback() {
+                    @Override
+                    public void onSuccess(SendResult sendResult) {
+                        countDownLatch.countDown();
+                        System.out.printf("%-10d OK %s %n", index, sendResult.getMsgId());
+                    }
+
+                    @Override
+                    public void onException(Throwable e) {
+                        countDownLatch.countDown();
+                        System.out.printf("%-10d Exception %s %n", index, e);
+                        e.printStackTrace();
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        countDownLatch.await(5, TimeUnit.SECONDS);
+        producer.shutdown();
+    }
+}
---
+++ mq/rocketmq/simple-example/src/main/java/com/demo/rocketmq/simple/Consumer.java
@@ -0,0 +1,45 @@
+package com.demo.rocketmq.simple;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
+
+/**
+ * @author luyi
+ * 同步消费者
+ */
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test");
+
+        // Specify name server addresses.
+        consumer.setNamesrvAddr("192.168.3.222:9876");
+
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("TopicTest", "*");
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+}
