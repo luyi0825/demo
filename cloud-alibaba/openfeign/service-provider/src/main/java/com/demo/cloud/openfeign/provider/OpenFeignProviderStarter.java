f4af2dc5113e004fca463aa7c3922d5d4eb8b14e
luyi <893431435@qq.com>
2021-10-20 22:47:49 +0800

    fix:清理无用的import

--- cloud-alibaba/openfeign/service-provider/src/main/java/com/demo/cloud/openfeign/provider/OpenFeignProviderStarter.java
+++ cloud-alibaba/openfeign/service-provider/src/main/java/com/demo/cloud/openfeign/provider/OpenFeignProviderStarter.java
@@ -2,18 +2,13 @@ package com.demo.cloud.openfeign.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * @author luyi
  * openfeign服务消费端
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class OpenFeignProviderStarter {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(OpenFeignProviderStarter.class);
