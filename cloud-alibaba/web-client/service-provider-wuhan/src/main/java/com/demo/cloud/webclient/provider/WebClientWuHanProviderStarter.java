0ec5cf5eeb276663b5564a56a51bd41aedf9d301
luyi <893431435@qq.com>
2021-10-20 18:23:27 +0800

    feat:初始化web-client-provider-wuhan

---
+++ cloud-alibaba/web-client/service-provider-wuhan/src/main/java/com/demo/cloud/webclient/provider/WebClientWuHanProviderStarter.java
@@ -0,0 +1,16 @@
+package com.demo.cloud.webclient.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luyi
+ * webclient服务提供方-上海
+ */
+@SpringBootApplication
+public class WebClientWuHanProviderStarter {
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(WebClientWuHanProviderStarter.class);
+        springApplication.run(args);
+    }
+}
