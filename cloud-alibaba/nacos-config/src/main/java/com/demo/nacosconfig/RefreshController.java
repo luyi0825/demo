1ee310e1e2c0bf3fdce05ab48111db0486a0a50b
luyi <893431435@qq.com>
2021-11-01 21:18:26 +0800

    nacosconfig

--- cloud-alibaba/nacos-config/src/main/java/com/demo/nacosconfig/RefreshController.java
+++ cloud-alibaba/nacos-config/src/main/java/com/demo/nacosconfig/RefreshController.java
@@ -1,8 +1,25 @@
 package com.demo.nacosconfig;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
 /**
  * @author luyi
- * xuan
+ * 刷新测试的controller
  */
+@RestController
+@RefreshScope
 public class RefreshController {
+
+    @Value("${name:}")
+    private String name;
+
+    @RequestMapping("/test")
+    public String test() {
+        return name;
+    }
 }
