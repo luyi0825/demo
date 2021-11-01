d225eca545445894913c67c675e33b97e3f96fca
luyi <893431435@qq.com>
2021-11-01 11:57:22 +0800

    feat:初始化sentinel

---
+++ cloud-alibaba/sentinel/sentinel-consumer/src/main/java/com/demo/sentinel/consumer/SentinelConsumerStarter.java
@@ -0,0 +1,14 @@
+package com.demo.sentinel.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luyi
+ */
+@SpringBootApplication
+public class SentinelConsumerStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelConsumerStarter.class, args);
+    }
+}
---
+++ cloud-alibaba/sentinel/sentinel-provider/src/main/java/com/demo/sentinel/provider/SentinelProviderStarter.java
@@ -0,0 +1,11 @@
+package com.demo.sentinel.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelProviderStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelProviderStarter.class, args);
+    }
+}
