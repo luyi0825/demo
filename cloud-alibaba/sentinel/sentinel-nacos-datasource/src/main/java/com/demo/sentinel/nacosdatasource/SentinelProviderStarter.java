d45657081e7eca0821bf7d22f6d7fc2e26d61c8f
893431435@qq.com <19940825Ls>
2021-11-15 22:33:13 +0800

    feat:sentinel利用nacos持久化

---
+++ cloud-alibaba/sentinel/sentinel-nacos-datasource/src/main/java/com/demo/sentinel/nacosdatasource/SentinelProviderStarter.java
@@ -0,0 +1,17 @@
+package com.demo.sentinel.nacosdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luyi
+ * sentinel服务提供者启动类
+ */
+@SpringBootApplication
+public class SentinelProviderStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelProviderStarter.class, args);
+    }
+
+
+}
