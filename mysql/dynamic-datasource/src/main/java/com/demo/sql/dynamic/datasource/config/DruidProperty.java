87196cbabcb86f3d20b94a84b0d819213faf898a
luyi <893431435@qq.com>
2022-05-07 19:25:54 +0800

    多数据源配置

---
+++ mysql/dynamic-datasource/src/main/java/com/demo/sql/dynamic/datasource/config/DruidProperty.java
@@ -0,0 +1,7 @@
+package com.demo.sql.dynamic.datasource.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("spring.")
+public class DruidProperty {
+}
