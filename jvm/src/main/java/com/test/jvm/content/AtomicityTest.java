ce4443c5df839307a76e81f72d9dc6e9b81eaff9
893431435@qq.com <19940825Ls>
2022-09-21 23:07:26 +0800

    原子类测试

---
+++ jvm/src/main/java/com/test/jvm/content/AtomicityTest.java
@@ -0,0 +1,22 @@
+package com.test.jvm.content;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 原子性测试
+ *
+ * @author luyi
+ */
+public class AtomicityTest {
+
+
+    public static void main(String[] args) {
+        long a = 1, b = 1;
+        a++;
+        ++b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+
+}
