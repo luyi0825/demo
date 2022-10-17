cf98bf5a9a61cde3acd3c2c16475d547d3c71a12
luyi <893431435@qq.com>
2022-10-17 19:07:07 +0800

    cas测试

--- jvm/src/main/java/com/test/jvm/Entity.java
+++ jvm/src/main/java/com/test/jvm/Entity.java
@@ -4,11 +4,36 @@ package com.test.jvm;
  * @author Administrator
  */
 public class Entity implements Cloneable {
+    private long x;
     private String name;
     private String age;
 
     @Override
     protected Entity clone() throws CloneNotSupportedException {
         return (Entity) super.clone();
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 }
