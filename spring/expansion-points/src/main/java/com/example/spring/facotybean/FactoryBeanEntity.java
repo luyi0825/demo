26a9785e16870a92056e135b43335c231b7938e4
luyi <893431435@qq.com>
2023-02-27 18:55:24 +0800

    spring拓展点-beanfactory

---
+++ spring/expansion-points/src/main/java/com/example/spring/facotybean/FactoryBeanEntity.java
@@ -0,0 +1,10 @@
+package com.example.spring.facotybean;
+
+public class FactoryBeanEntity {
+
+    private String name;
+
+    public FactoryBeanEntity(String name) {
+        this.name = name;
+    }
+}
